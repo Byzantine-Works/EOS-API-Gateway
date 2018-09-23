34a9c7c0e7d850f1377781ad7c5bd349978025e6
Reddy <manjunath.reddy@bottomline.com>
2018-09-23 07:52:56 -0700

    Format fixes

--- api/controllers/decipher.js
+++ api/controllers/decipher.js
@@ -5,11 +5,10 @@ var iv = Buffer.from('0000000000000000');
 //This function decrypts the nonce and private key using the 
 //Common salt exchanged between the customer and api
 var decrypt256 = function (data, key) {
-    //this key is the security key and must be passed as -X args?
+    //this key is the security key and must be passed as -X args to swagger?
     key = "bf32eb1e0b28d4b75bb1da9eaa4c5b02";
     var cipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
     var decryptedData = cipher.update(data, 'hex', 'utf8') + cipher.final('utf8');
-    
     var cipherTokens = decryptedData.split(" ");
     console.log('Decrypted 256 enc with nonce:key => ' + cipherTokens[0] + ":" + cipherTokens[1]);
     if (cipherTokens[0] > client.getNonce(key, cipherTokens[0]))
