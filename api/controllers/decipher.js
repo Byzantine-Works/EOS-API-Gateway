ce14d95e204b9661e354d97c52c061b9a9bc897d
Reddy <manjunath.reddy@bottomline.com>
2018-09-22 23:32:26 -0700

    Fixed 256 hash issue plaguing java/nodejs encrypt decrypt functions

--- api/controllers/decipher.js
+++ api/controllers/decipher.js
@@ -1,23 +1,20 @@
 const client = require('./clients.js');
 var crypto = require('crypto');
-var iv = new Buffer('0000000000000000');
+var iv = Buffer.from('0000000000000000');
 
 //This function decrypts the nonce and private key using the 
 //Common salt exchanged between the customer and api
 var decrypt256 = function (data, key) {
+    //this key is the security key and must be passed as -X args?
     key = "bf32eb1e0b28d4b75bb1da9eaa4c5b02";
-    var encodeKey = crypto.createHash('sha256').update(key, 'utf-8').digest()
-    var cipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encodeKey, 'hex'),
-        Buffer.from(iv));
+    var cipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
     var decryptedData = cipher.update(data, 'hex', 'utf8') + cipher.final('utf8');
     
     var cipherTokens = decryptedData.split(" ");
-    console.log('Decrypted 128 enc with nonce:key => ' + cipherTokens[0] + ":" + cipherTokens[1]);
+    console.log('Decrypted 256 enc with nonce:key => ' + cipherTokens[0] + ":" + cipherTokens[1]);
     if (cipherTokens[0] > client.getNonce(key, cipherTokens[0]))
         return cipherTokens[1];
     else
         throw new Error('nonce too low!');
 };
-module.exports.decryptXStrong  = decrypt256;
-
-  
+module.exports.decryptXStrong = decrypt256;
