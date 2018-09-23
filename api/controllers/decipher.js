766dab7109b9b2587f98123c186387c5749ba739
VernSul <vicfaucon@gmail.com>
2018-09-22 17:49:34 -0700

    Changed the decrypt function from 128 to 256

--- api/controllers/decipher.js
+++ api/controllers/decipher.js
@@ -4,12 +4,13 @@ var iv = new Buffer('0000000000000000');
 
 //This function decrypts the nonce and private key using the 
 //Common salt exchanged between the customer and api
-var decrypt128 = function (data, key) {
+var decrypt256 = function (data, key) {
     key = "bf32eb1e0b28d4b75bb1da9eaa4c5b02";
-    var encodeKey = crypto.createHash('sha256').update(key, 'utf-8').digest();
-    var cipher = crypto.createDecipheriv('aes-128-cbc', Buffer.from(key, 'hex'),
+    var encodeKey = crypto.createHash('sha256').update(key, 'utf-8').digest()
+    var cipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encodeKey, 'hex'),
         Buffer.from(iv));
     var decryptedData = cipher.update(data, 'hex', 'utf8') + cipher.final('utf8');
+    
     var cipherTokens = decryptedData.split(" ");
     console.log('Decrypted 128 enc with nonce:key => ' + cipherTokens[0] + ":" + cipherTokens[1]);
     if (cipherTokens[0] > client.getNonce(key, cipherTokens[0]))
@@ -17,4 +18,6 @@ var decrypt128 = function (data, key) {
     else
         throw new Error('nonce too low!');
 };
-module.exports.decryptXStrong  = decrypt128;
+module.exports.decryptXStrong  = decrypt256;
+
+  
