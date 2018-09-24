c5a724eea6df4725fb9678df99486322414c207b
VernSul <vicfaucon@gmail.com>
2018-09-24 12:29:24 -0700

    Made the json file on the server side actually recording data change

--- thinWallet_Client/server/server.js
+++ thinWallet_Client/server/server.js
@@ -2,14 +2,17 @@ const io = require('socket.io')();
 const db = require('./nonce.json')
 const fs = require('fs');
 
-let nonce = db.nonce;
+
 
 io.on('connection', (client) => {
     client.on('user', (data) => {
         // if(!db[data[0]]) db[data[0]] = 0;
-        nonce ++;
-        client.emit(data[1], nonce);
-        fs.writeFileSync('./nonce.json', db);
+        console.log(db)
+        db.nonce ++;
+        fs.writeFileSync('./nonce.json', JSON.stringify(db), 'utf8');
+        client.emit(data[1], db.nonce);
+        console.log(db);
+        
 
     });
 });
